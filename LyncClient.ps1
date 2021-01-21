78b9a137bc1311b1aa6a7e9ca9de64841cf94be9
Matt Himrod <himrodma@upmc.edu>
2021-01-21 11:27:35 -0500

    Added exception handling.

--- LyncClient.ps1
+++ LyncClient.ps1
@@ -2,12 +2,25 @@ $Url = "http://busylight.lan:5000/api/presence"
 $Delay = 15
 
 Add-Type –Path "C:\Program Files (x86)\Microsoft Office 2013\LyncSDK\Assemblies\Desktop\Microsoft.Lync.Model.dll";
-$Lync = [Microsoft.Lync.Model.LyncClient]::GetClient()
+try {
+    $Lync = [Microsoft.Lync.Model.LyncClient]::GetClient()
+} catch [System.Management.Automation.MethodInvocationException] {
+    Start-Process "C:\Program Files (x86)\Microsoft Office\root\Office16\lync.exe"
+    start-sleep –Seconds $Delay
+    $Lync = [Microsoft.Lync.Model.LyncClient]::GetClient()
+}
 
 $LastActivity = ""
 
 while($true){
-    $activity = $Lync.Self.Contact.GetContactInformation("ActivityId")
+    try {
+        $activity = $Lync.Self.Contact.GetContactInformation("ActivityId")
+    } catch {
+        Start-Process "C:\Program Files (x86)\Microsoft Office\root\Office16\lync.exe"
+        start-sleep –Seconds $Delay 
+        $Lync = [Microsoft.Lync.Model.LyncClient]::GetClient()
+    }
+    
     if ($activity -ne $LastActivity) {
         Invoke-RestMethod -Uri $Url -Method 'Post' -Body @{ state = $activity }
         $LastActivity = $activity
