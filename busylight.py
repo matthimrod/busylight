220665df12c2b5ca077743f894e4e6f92db29209
Matt Himrod <matt@matthimrod.com>
2021-01-25 17:21:54 -0500

    Enhanced the preview/manual control page

--- busylight.py
+++ busylight.py
@@ -31,16 +31,16 @@ def set_state(text):
         print(f"Undefined state {text}.")
 
 def set_color(color):
-    if test_mode: 
-        return
-    elif color == 'off':
+    if color == 'off':
         status['color']='off'
-        unicornhatmini.clear()
-        unicornhatmini.show()
+        if not test_mode: 
+            unicornhatmini.clear()
+            unicornhatmini.show()
     elif color in config['colors']:
         status['color']=color
-        unicornhatmini.set_all(config['colors'][color][0],config['colors'][color][1],config['colors'][color][2])
-        unicornhatmini.show()
+        if not test_mode: 
+            unicornhatmini.set_all(config['colors'][color][0],config['colors'][color][1],config['colors'][color][2])
+            unicornhatmini.show()
 
 def get_color():
     if color in status:
@@ -51,6 +51,52 @@ def get_color():
 @app.route('/heartbeat', methods=['GET'])
 def heartbeat():
     return '200 - OK'
+
+@app.route('/api/config', methods=['GET'])
+def get_config():
+    return jsonify(config)
+
+@app.route('/api/state', methods=['GET'])
+def get_state():
+    return jsonify(status)
+
+@app.route('/api/reload', methods=['GET'])
+def load_config():
+    global config
+    with open('config.json', 'r') as read_file:
+        config = json.load(read_file)
+    if 'brightness' in config and not test_mode:
+        unicornhatmini.set_brightness(config['brightness'])
+    return '200 - OK'
+
+@app.route('/api/presence', methods=['POST'])
+def set_presence():
+    if request.form.get('state'):
+        status['presence'] = request.form.get('state').lower()
+        if not status['override']:
+            set_state(status['presence'])
+        return get_return()
+    else:
+        return f"Unable to process request.", 400
+
+@app.route('/api/override', methods=['POST'])
+def set_override():
+    if request.form.get('state'):
+        status['override'] = request.form.get('state').lower()
+        set_state(status['override'])
+        return get_return()
+    elif request.form.get('clear').lower() == 'true':
+        status['override'] = None
+        set_state(status['presence'])
+        return get_return()
+    else:
+        return f"Unable to process request.", 400
+
+def get_return():
+    if request.form.get('redirect') == 'true':
+        return redirect('/')
+    else:
+        return jsonify(status)
 
 @app.route('/')
 def root():
@@ -94,67 +140,36 @@ def root():
                 <rect width="300" height="120" style="fill:{};stroke-width:3;stroke:black" />
             </svg>
         </p>
+        <h1 align='center'>Presence</h1>
         <p>
-            <form action="http://busylight.lan:5000/api/presence" method="post">
-                <input class='btn' type="submit" name="state" value="purple" />
-                <input class='btn' type="submit" name="state" value="blue" />
-                <input class='btn' type="submit" name="state" value="green" />
-                <input class='btn' type="submit" name="state" value="yellow" />
-                <input class='btn' type="submit" name="state" value="orange" />
-                <input class='btn' type="submit" name="state" value="red" />
-                <input class='btn' type="submit" name="state" value="off" />
+            <form action="/api/presence" method="post">
+                <button class='btn' type="submit" name="state" value="purple">purple</button>
+                <button class='btn' type="submit" name="state" value="blue">blue</button>
+                <button class='btn' type="submit" name="state" value="green">green</button>
+                <button class='btn' type="submit" name="state" value="yellow">yellow</button>
+                <button class='btn' type="submit" name="state" value="orange">orange</button>
+                <button class='btn' type="submit" name="state" value="red">red</button>
+                <button class='btn' type="submit" name="state" value="off">off</button>
+                <input type='hidden' name='redirect' value='true' />
+            </form>
+        </p>
+        <h1 align='center'>Override</h1>
+        <p>
+            <form action="/api/override" method="post">
+                <button class='btn' type="submit" name="state" value="purple">purple</button>
+                <button class='btn' type="submit" name="state" value="blue">blue</button>
+                <button class='btn' type="submit" name="state" value="green">green</button>
+                <button class='btn' type="submit" name="state" value="yellow">yellow</button>
+                <button class='btn' type="submit" name="state" value="orange">orange</button>
+                <button class='btn' type="submit" name="state" value="red">red</button>
+                <button class='btn' type="submit" name="state" value="off">off</button>
+                <button class='btn' type="submit" name="clear" value="true">clear</button>
                 <input type='hidden' name='redirect' value='true' />
             </form>
         </p>
     </body>
 </html>
 """.format(current)
-
-@app.route('/api/config', methods=['GET'])
-def get_config():
-    return jsonify(config)
-
-@app.route('/api/state', methods=['GET'])
-def get_state():
-    return jsonify(status)
-
-@app.route('/api/reload', methods=['GET'])
-def load_config():
-    global config
-    with open('config.json', 'r') as read_file:
-        config = json.load(read_file)
-    if 'brightness' in config and not test_mode:
-        unicornhatmini.set_brightness(config['brightness'])
-    return '200 - OK'
-
-@app.route('/api/presence', methods=['POST'])
-def set_presence():
-    if request.form.get('state'):
-        status['presence'] = request.form.get('state').lower()
-        if not status['override']:
-            set_state(status['presence'])
-            if request.form.get('redirect') == 'true':
-                return redirect('/')
-            else:
-                return f"Presence {status['presence']} received."
-        else:
-            return f"Presence {status['presence']} received. Override {status['override']} active."
-    else:
-        return f"Unable to process request.", 400
-
-@app.route('/api/override', methods=['POST'])
-def set_override():
-    if request.form.get('state'):
-        status['override'] = request.form.get('state').lower()
-        set_state(status['override'])
-        return f"Override {status['override']} received."
-    elif request.form.get('clear').lower() == 'true':
-        status['override'] = None
-        set_state(status['presence'])
-        return f"Override cleared. Presence {status['presence']} set."
-    else:
-        return f"Unable to process request.", 400
-
 
 if __name__ == '__main__':
     load_config()
