04986cfd1c9f27e31c281eaefcdfa7ba60556d3f
Matt Himrod <matt@matthimrod.com>
2022-07-13 13:21:26 -0400

    Added window title, output timestamp, and only sending when status changes.

--- TeamsClient.ps1
+++ TeamsClient.ps1
@@ -3,20 +3,26 @@ $Url = "http://busylight.lan:5000/api/presence"
 $MaxRetry = 5
 $RetryWait = 5
 $Delay = 60
+$Host.UI.RawUI.WindowTitle = "BusyLight Client"
+
+function Get-TimeStamp {
+    return "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
+}
+
+$LastActivity = ""
 
 while($true){
 
-    $LogContent = Get-Content $TeamsLogFile -tail 1000
-    $Selected = ($LogContent | Select-String -Pattern 'StatusIndicatorStateService\: Added (\w+) [^|]*')
-    $activity = $Selected.Matches[$Selected.Matches.Length - 1].Groups[1].Value
-    Write-Output "Found $activity"
+    $LogContent = Get-Content $TeamsLogFile -tail 1000 | Select-String -Pattern 'StatusIndicatorStateService\: Added (\w+) [^|]*')
+    $activity = $LogContent.Matches[$LogContent.Matches.Length - 1].Groups[1].Value
 
-    if ($null -ne $activity) {
+    if ($null -ne $activity -and $activity -ne $LastActivity) {
         $retries = $MaxRetry
         do {
-            Write-Log "Setting status to $activity"
+            Write-Output "$(Get-TimeStamp) Setting status to $activity"
             try {
-                Invoke-RestMethod -Uri $Url -Method 'Post' -Body @{ state = $activity }
+                $result = Invoke-RestMethod -Uri $Url -Method 'Post' -Body @{ state = $activity }
+                $LastActivity = $activity
                 $retries = 0
             } catch [System.Object] {
                 $retries--
