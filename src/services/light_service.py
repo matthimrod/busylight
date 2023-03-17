f4532987e224d6b0af3719d8648956baee16d7ff
Matt Himrod <matt@matthimrod.com>
2023-03-17 09:15:35 -0400

    Adding tests

--- src/services/light_service.py
+++ src/services/light_service.py
@@ -1,5 +1,4 @@
 import re
-from typing import Optional, Union
 
 from unicornhatmini import UnicornHATMini
 
@@ -13,14 +12,19 @@ class LightService:
     brightness: float
     color_map: dict
     status_map: dict
-    state: Optional[str]
-    override: Optional[str]
+    state: str
+    override: str
     _colorBlue: int
     _colorGreen: int
     _colorRed: int
     _test_mode: bool
 
     def __init__(self, **kwargs) -> None:
+        self._colorBlue = 0
+        self._colorGreen = 0
+        self._colorRed = 0
+        self.state = None
+        self.override = None
         self.brightness = float(kwargs.get('brightness',
                                 kwargs.get('config', {}).get('brightness', 1.0)))
         self.color_map = kwargs.get('config', {}).get('colors', {})
@@ -30,12 +34,15 @@ class LightService:
         if not self._test_mode:
             self.unicornhatmini = UnicornHATMini()
             self.unicornhatmini.clear()
+            self.unicornhatmini.set_brightness(self.brightness)
             self.unicornhatmini.show()
 
-        if 'status' in kwargs:
-            self.set_status(str(kwargs.get('status')))
+        self.set_status(str(kwargs.get('init_status', '')))
 
     def _update(self, red: int, green: int, blue: int) -> None:
+        if(red < 0 or red > 255 or green < 0 or green > 255 or blue < 0 or blue > 255):
+            raise ValueError()
+
         self._colorRed = red
         self._colorGreen = green
         self._colorBlue = blue
@@ -44,37 +51,39 @@ class LightService:
             self.unicornhatmini.set_all(self._colorRed,
                                         self._colorGreen,
                                         self._colorBlue)
+            self.unicornhatmini.set_brightness(self.brightness)
             self.unicornhatmini.show()
 
     def _set_rgb(self, rgb_value: str) -> None:
-        result = re.match(r'#([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})', rgb_value)
+        result = re.match(r'#([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})$', rgb_value)
         if result:
             self._update(int(result.group(1), 16),
                          int(result.group(2), 16),
                          int(result.group(3), 16))
+        else:
+            raise ValueError()
 
     def _set_color_name(self, color_name_value: str) -> None:
         if color_name_value in self.color_map:
-            if re.match(r'#[0-9a-fA-F]{6}', self.color_map.get(color_name_value, '')):
-                self._set_rgb(self.color_map.get(color_name_value, ''))
-
-            elif isinstance(self.color_map.get(color_name_value), dict):
+            if isinstance(self.color_map.get(color_name_value), list):
                 self._update(self.color_map[color_name_value][0],
                              self.color_map[color_name_value][1],
                              self.color_map[color_name_value][2])
+            elif re.match(r'#[0-9a-fA-F]{6}$', self.color_map.get(color_name_value, '')):
+                self._set_rgb(self.color_map.get(color_name_value, ''))
 
     def _set_state_name(self, state_value: str) -> None:
         if state_value in self.status_map:
-            if re.match(r'#[0-9a-fA-F]{6}', self.status_map.get(state_value, '')):
-                self._set_rgb(self.status_map.get(state_value, ''))
+            if isinstance(self.status_map.get(state_value), list):
+                self._update(self.status_map[state_value][0],
+                             self.status_map[state_value][1],
+                             self.status_map[state_value][2])
 
             elif self.status_map.get(state_value, '') in self.color_map:
                 self._set_color_name(self.status_map.get(state_value, ''))
 
-            elif isinstance(self.status_map.get(state_value), dict):
-                self._update(self.status_map[state_value][0],
-                             self.status_map[state_value][1],
-                             self.status_map[state_value][2])
+            elif re.match(r'#[0-9a-fA-F]{6}$', self.status_map.get(state_value, '')):
+                self._set_rgb(self.status_map.get(state_value, ''))
 
     def set_status(self, value: str) -> None:
         if value in self.status_map:
@@ -87,7 +96,7 @@ class LightService:
             if not self.override:
                 self._set_color_name(str(self.state))
 
-        elif re.match(r'#[0-9a-fA-F]{6}', str(self.state)):
+        elif re.match(r'#[0-9a-fA-F]{6}$', str(self.state)):
             self.state = value
             if not self.override:
                 self._set_rgb(str(self.state))
@@ -101,7 +110,7 @@ class LightService:
             self.override = value
             self._set_color_name(str(self.state))
 
-        elif re.match(r'#[0-9a-fA-F]{6}', str(self.state)):
+        elif re.match(r'#[0-9a-fA-F]{6}$', str(self.state)):
             self.override = value
             self._set_rgb(str(self.state))
 
@@ -114,5 +123,5 @@ class LightService:
             elif self.state in self.color_map:
                 self._set_color_name(self.state)
 
-            elif re.match(r'#[0-9a-fA-F]{6}', str(self.state)):
+            elif re.match(r'#[0-9a-fA-F]{6}$', str(self.state)):
                 self._set_rgb(self.state)
